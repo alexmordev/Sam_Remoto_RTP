d663bd1991c7cc855666692776b1ac5f82882377
Omar Castro <omarsc97@outlook.com>
2022-06-03 12:13:15 -0500

    Terminando el footer, header, pagias de SAMcounters

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,12 +1,26 @@
-import React from 'react'
+import React from 'react';
+
 
 
 export const Footer = () => {
+
+
   return (
-    <>
-      
-    Footer
+    <div className='absolute border-3 border-green-200  w-full h-4rem  bottom-0'>
+
+      <div className='flex justify-content-center align-content-center'>
+
+        <p className='font-normal text-900 text-center'>
+        El uso indebido de este software puede representar un delito
+        y es responsabilidad completa del usuario que lo lleve a cabo. 
+        En caso de tener dudas sobre sus funciones favor de contactar
+        al equipo de desarrollo de RTP
+        </p>
+
+      </div>
+
+       
     
-    </>
+    </div>
   )
 }
