cb08f4b73d0e1f0b8f940eee2eed62ab96f4dc6d
Omar Castro <omarsc97@outlook.com>
2022-06-03 09:36:04 -0500

    Cambios, no me acuerdo cuales

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,17 +1,41 @@
 import React from 'react';
-import { Navigator } from './../Navigator';
 import { LogoHeader } from '../LogoHeader';
+import { Button } from 'primereact/button';
+import { LogoutButton } from '../LogoutButton';
+import { Toolbar } from 'primereact/toolbar';
+import { SplitButton } from 'primereact/splitbutton';
 
 
 
 export const Header = () => {
+  const items = [
+    {
+      label: 'Dumps',
+      icon: 'pi-angle-right',
+    },
+    {
+      label: 'Contadores',
+      icon: 'pi-angle-right',
+    }
+  ]
 
-  
+  const left_items = (
+    <>
+      <Button label='Inicio' className='p-button-link'/>
+      <Button label='Aplicación' className='p-button-link'/>
+      <SplitButton label='Gestión' className='p-button-link bg-white' model={items}></SplitButton>     
+    </>
+  )
+
+    
+
+
 
   return (
-    <div className="w-screen h-4rem bg-green-500">
-      <LogoHeader className="w-8rem" />
-      <Navigator />
+    <div className="w-screen h-4rem ">
+
+      <Toolbar className='w-screen absolute top-0 right-0' left={left_items} right={<LogoutButton/>}/>
+
       
 
     </div>
