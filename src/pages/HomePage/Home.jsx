f357cb87a1bf57b9487bedcbf7d57ced2a6f5a25
Omar Castro <omarsc97@outlook.com>
2022-06-07 11:50:56 -0500

    Agregando chart

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,42 +1,37 @@
-import React, { useState } from 'react';
-import { Chart } from 'primereact/chart';
-import { Container } from './../../Components/Container/Container';
+import React, { useState } from "react";
+import { Chart } from "primereact/chart";
+import { Container } from "./../../Components/Container/Container";
 
 export const Home = () => {
-
   const [chartData] = useState({
-    labels: ['A', 'B', 'C'],
+    labels: ["A", "B", "C"],
     datasets: [
-        {
-            data: [300, 50, 100],
-            backgroundColor: [
-                "#42A5F5",
-                "#66BB6A",
-                "#FFA726"
-            ],
-            hoverBackgroundColor: [
-                "#64B5F6",
-                "#81C784",
-                "#FFB74D"
-            ]
-        }
-    ]
-});
+      {
+        data: [300, 50, 100],
+        backgroundColor: ["#42A5F5", "#66BB6A", "#FFA726"],
+        hoverBackgroundColor: ["#64B5F6", "#81C784", "#FFB74D"],
+      },
+    ],
+  });
 
-const [lightOptions] = useState({
+  const [lightOptions] = useState({
     plugins: {
-        legend: {
-            labels: {
-                color: '#495057'
-            }
-        }
-    }
-});
+      legend: {
+        labels: {
+          color: "#495057",
+        },
+      },
+    },
+  });
 
   return (
     <Container>
-      <Chart type="pie" data={chartData} options={lightOptions} style={{ position: 'absolute', width: '40%' }} />
-
+        <Chart
+          type="pie"
+          data={chartData}
+          options={lightOptions}
+          style={{ position: 'relative', width: '40%' }}
+          />
     </Container>
-  )
-}
+  );
+};
