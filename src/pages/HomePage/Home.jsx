c8a97bf362c8f17cf8e601594b66a68199037585
Omar Castro <omarsc97@outlook.com>
2022-06-07 10:17:40 -0500

    HomePage

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,12 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Chart } from 'primereact/chart';
 import { Header } from '../../Components/Header/Header'
 
 export const Home = () => {
+
+  const [chartData] = useState({
+    labels: ['A', 'B', 'C'],
+    datasets: [
+        {
+            data: [300, 50, 100],
+            backgroundColor: [
+                "#42A5F5",
+                "#66BB6A",
+                "#FFA726"
+            ],
+            hoverBackgroundColor: [
+                "#64B5F6",
+                "#81C784",
+                "#FFB74D"
+            ]
+        }
+    ]
+  });
+
+  const [lightOptions] = useState({
+    plugins: {
+        legend: {
+            labels: {
+                color: '#495057'
+            }
+        }
+    }
+  });
+
   return (
-    <div className='w-full h-full'>
+    <div className='card flex justify-content-center'>
       <Header />
-
-
+      <Chart type="pie" data={chartData} options={lightOptions} style={{ position: 'relative', width: '40%' }} />
 
 
     </div>
