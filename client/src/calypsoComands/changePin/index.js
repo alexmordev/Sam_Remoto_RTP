166d7964ccc491e492e96d310317657d59fcdd04
Omar Castro <omarsc97@outlook.com>
2022-07-15 10:23:20 -0500

    Cascaron de peticiones Calypso

--- client/src/calypsoComands/changePin/index.js
+++ client/src/calypsoComands/changePin/index.js
@@ -7,6 +7,16 @@ import giveRandom from '../giveRandom';
 import cardCipherPinUpdate from '../cardCipherPinUpdate';
 import changePin from '../changePin';
 
+
+
+// Primero se ejecuta selectAplication al local serve el cual es un metodo GET, se obtiene el SN y se le agrega 08 al principio
+// Se ejecuta diversifier al remote serve es un metodo POST y solo mostrar el status de esa operacion
+// se ejecuta getChallenge al local serve es un metodo GET y se obtiene el Response.slice(a,-4)
+// se ejectuva giveRandom al remote serve es un metodo POST y se le manda el response de getChallenge
+// se ejecuta cardCipherPinUpdate al remote serve es un metodo post y se le manda en ascii el nuevo pin
+// se ejecuta changePin al local serve es un metodo POST y se manda el response de cardCipherPinUpdate
+
+
 const index = () => {
 
   const respSelectAplication = "0800000000946AD0F0";
