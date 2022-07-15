ce3579b3909742d06a822eb80f44e0ef93f60ba1
Alejandro Morales <alejandromorjim@gmail.com>
2022-07-15 15:09:36 -0500

    Rehabilitate not complete

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,9 +1,11 @@
 // const express = require('express');
 const { Router } = require('express'); 
-const router = Router();
-module.exports = router;
-
 const { SelectApplication } = require ('../controllers/Temp/SelectApplication');
+// const { OpenSecureSession } = require ('../controllers/Temp/OpenSecureSession');
+const router = Router();
 
-router.get('/', (req, res) => { res.json({ Welcome:'user!' }) })
 router.get('/selectApp', SelectApplication);
+// router.get('/openSecureSession', OpenSecureSession );
+
+
+module.exports = router;
